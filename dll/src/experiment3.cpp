10b4c85bf7d2afe66313c4695e82a21e0f062945
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-12-13 10:55:36 +0100

    DLL experiment 3

---
+++ dll/src/experiment3.cpp
@@ -0,0 +1,31 @@
+//=======================================================================
+// Copyright (c) 2016 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#include <iostream>
+#include <chrono>
+
+#include "dll/rbm/rbm.hpp"
+#include "dll/dbn.hpp"
+
+#include "mnist/mnist_reader.hpp"
+#include "mnist/mnist_utils.hpp"
+
+int main(int /*argc*/, char* /*argv*/ []) {
+    auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<float, 1, 28, 28>>();
+
+    mnist::binarize_dataset(dataset);
+
+    dll::rbm_desc<28 * 28, 500, dll::batch_size<100>, dll::momentum>::layer_t rbm;
+
+    rbm.learning_rate = 0.1;
+    rbm.initial_momentum = 0.9;
+    rbm.final_momentum = 0.9;
+
+    rbm.train(dataset.training_images, 50);
+
+    return 0;
+}

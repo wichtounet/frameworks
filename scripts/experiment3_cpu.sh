dbb6d9562263dd72a949b5e552d798c5441e5bf2
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-12-15 14:36:02 +0100

    Scripts for experiment 3

---
+++ scripts/experiment3_cpu.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#=======================================================================
+# Copyright (c) 2016 Baptiste Wicht
+# Distributed under the terms of the MIT License.
+# (See accompanying file LICENSE or copy at
+#  http://opensource.org/licenses/MIT)
+#=======================================================================
+
+######################
+# Experiment 1 (CPU) #
+######################
+
+exp=3
+mode=cpu
+
+echo "Starting experiment 3 (CPU)"
+
+#  DLL  #
+#########
+
+echo "Starting DLL"
+
+mkdir -p results/$exp/$mode/dll
+
+cd dll/
+
+# Set variables for performance
+export DLL_BLAS_PKG=mkl-threads
+export ETL_MKL=true
+make clean > /dev/null
+make release/bin/experiment3 > /dev/null
+before=`date "+%s"`
+./release/bin/experiment3 | tee ../results/$exp/$mode/dll/raw_results
+after=`date "+%s"`
+echo "Time: $((after - before))"
+
+# Cleanup variables
+unset DLL_BLAS_PKG
+unset ETL_MKL
+
+cd ..
+
+#  TF  #
+########
+
+echo "Starting TensorFlow"
+
+mkdir -p results/$exp/$mode/tf
+
+cd tf
+
+source ~/.virtualenvs/tf/bin/activate
+
+before=`date "+%s"`
+CUDA_VISIBLE_DEVICES=-1 python experiment3.py | tee ../results/$exp/$mode/tf/raw_results
+after=`date "+%s"`
+echo "Time: $((after - before))"
+
+deactivate
+
+cd ..
---
+++ scripts/experiment3_gpu.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+#=======================================================================
+# Copyright (c) 2016 Baptiste Wicht
+# Distributed under the terms of the MIT License.
+# (See accompanying file LICENSE or copy at
+#  http://opensource.org/licenses/MIT)
+#=======================================================================
+
+######################
+# Experiment 1 (GPU) #
+######################
+
+exp=3
+mode=gpu
+
+echo "Starting experiment 3 (GPU)"
+
+#  DLL  #
+#########
+
+echo "Starting DLL"
+
+mkdir -p results/$exp/$mode/dll
+
+cd dll/
+
+# Set variables for performance
+export DLL_BLAS_PKG=mkl-threads
+export ETL_MKL=true
+export ETL_CUBLAS=true
+export ETL_CUDNN=true
+export ETL_CUFFT=true
+make clean > /dev/null
+make release/bin/experiment3 > /dev/null
+before=`date "+%s"`
+./release/bin/experiment3 | tee ../results/$exp/$mode/dll/raw_results
+after=`date "+%s"`
+echo "Time: $((after - before))"
+
+# Cleanup variables
+unset ETL_CUFFT
+unset ETL_CUDNN
+unset ETL_CUBLAS
+unset ETL_MKL
+unset DLL_BLAS_PKG
+
+cd ..
+
+#  TF  #
+########
+
+echo "Starting TensorFlow"
+
+mkdir -p results/$exp/$mode/tf
+
+cd tf
+
+source ~/.virtualenvs/tf/bin/activate
+
+before=`date "+%s"`
+CUDA_VISIBLE_DEVICES=0 python experiment3.py | tee ../results/$exp/$mode/tf/raw_results
+after=`date "+%s"`
+echo "Time: $((after - before))"
+
+deactivate
+
+cd ..

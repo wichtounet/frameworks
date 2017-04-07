98be9f9220d2be4312881c673bb188533a1e9ad6
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-04-07 16:30:53 +0200

    Script for experiment 5 on CPU

---
+++ scripts/experiment5_cpu.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+#=======================================================================
+# Copyright (c) 2016 Baptiste Wicht
+# Distributed under the terms of the MIT License.
+# (See accompanying file LICENSE or copy at
+#  http://opensource.org/licenses/MIT)
+#=======================================================================
+
+######################
+# Experiment 5 (CPU) #
+######################
+
+exp=5
+mode=cpu
+
+echo "Starting experiment $exp ($mode)"
+
+#  DLL  #
+#########
+
+echo "Starting DLL"
+
+mkdir -p results/$exp/$mode/dll
+
+cd dll/
+
+# Set variables for performance
+export DLL_BLAS_PKG=mkl
+export ETL_MKL=true
+make clean > /dev/null
+make release/bin/experiment5 > /dev/null
+before=`date "+%s"`
+./release/bin/experiment5 | tee ../results/$exp/$mode/dll/raw_results
+after=`date "+%s"`
+echo "Time: $((after - before))"
+
+# Cleanup variables
+unset DLL_BLAS_PKG
+unset ETL_MKL
+
+cd ..
+
+#  TF  #
+########
+
+echo "Starting TensorFlow"
+
+mkdir -p results/$exp/$mode/tf
+
+cd tf
+
+source ~/.virtualenvs/tf2/bin/activate
+
+before=`date "+%s"`
+CUDA_VISIBLE_DEVICES=-1 python experiment5.py | tee ../results/$exp/$mode/tf/raw_results
+after=`date "+%s"`
+echo "Time: $((after - before))"
+
+deactivate
+
+cd ..
+
+#  Keras  #
+###########
+
+echo "Starting Keras"
+
+mkdir -p results/$exp/$mode/keras
+
+cd keras
+
+source ~/.virtualenvs/tf2/bin/activate
+
+before=`date "+%s"`
+CUDA_VISIBLE_DEVICES=-1 python experiment5.py | tee ../results/$exp/$mode/keras/raw_results
+after=`date "+%s"`
+echo "Time: $((after - before))"
+
+deactivate
+
+cd ..
+
+#  DeepLearning4J  #
+####################
+
+echo "Starting DeepLearning4j"
+
+mkdir -p results/$exp/$mode/dl4j
+
+cd dl4j
+
+export DL4J_MODE=native
+mvn clean install > /dev/null
+
+cd target/classes
+
+before=`date "+%s"`
+java -cp ../ihatejava-0.7-SNAPSHOT-bin.jar wicht.experiment5 | tee ../results/$exp/$mode/dl4j/raw_results
+after=`date "+%s"`
+echo "Time: $((after - before))"
+
+cd ../..
+
+cd ..
+
+#  Caffe  #
+###########
+
+echo "Starting Caffe"
+
+mkdir -p results/$exp/$mode/caffe
+
+cd caffe
+
+export CAFFE_ROOT="/home/wichtounet/dev/caffe-cpu"
+
+before=`date "+%s"`
+$CAFFE_ROOT/build/tools/caffe train --solver=experiment5_solver.prototxt | tee ../results/$exp/$mode/caffe/raw_results
+after=`date "+%s"`
+echo "Time: $((after - before))"
+
+cd ..
+
+#  Torch  #
+###########
+
+echo "Starting Torch"
+
+mkdir -p results/$exp/$mode/torch
+
+cd torch
+
+source ~/torch/install/bin/torch-activate
+
+before=`date "+%s"`
+th experiment5.lua | tee ../results/$exp/$mode/torch/raw_results
+after=`date "+%s"`
+echo "Time: $((after - before))"
+
+cd ..

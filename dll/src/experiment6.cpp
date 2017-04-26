ce1dd84c3f9d386823bef512d12a37d733b09ef5
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-04-26 09:39:13 +0200

    Prepare DLL experiment6

---
+++ dll/src/experiment6.cpp
@@ -0,0 +1,28 @@
+//=======================================================================
+// Copyright (c) 2016 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#include <iostream>
+#include <chrono>
+
+#include "dll/neural/conv_layer.hpp"
+#include "dll/neural/conv_same_layer.hpp"
+#include "dll/neural/dense_layer.hpp"
+#include "dll/pooling/mp_layer.hpp"
+#include "dll/dbn.hpp"
+#include "dll/trainer/stochastic_gradient_descent.hpp"
+
+static constexpr const char* imagenet_path = "/data_ext/imagenet/resized/";
+
+int main(int /*argc*/, char* /*argv*/ []) {
+    // 0. Read list of files with the label
+
+
+
+    //TODO
+
+    return 0;
+}

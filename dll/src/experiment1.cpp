3e7fcd1afa67e36731f0fdb302e4fa41796a4025
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-11-10 16:13:21 +0100

    Fix DLL sources

--- dll/src/experiment1.cpp
+++ dll/src/experiment1.cpp
@@ -22,9 +22,10 @@ int main(int /*argc*/, char* /*argv*/ []) {
 
     using dbn_t = dll::dbn_desc<
         dll::dbn_layers<
-            dll::dense_desc<28 * 28, 500>::layer_t,
-            dll::dense_desc<500, 250>::layer_t,
-            dll::dense_desc<250, 10, dll::activation<dll::function::SOFTMAX>>::layer_t>,
+            dll::dense_layer<28 * 28, 500>,
+            dll::dense_layer<500, 250>,
+            dll::dense_layer<250, 10, dll::softmax>
+        >,
         dll::batch_size<100>,
         dll::updater<dll::updater_type::MOMENTUM>
         >::dbn_t;
--- dll/src/experiment2.cpp
+++ dll/src/experiment2.cpp
@@ -24,12 +24,12 @@ int main(int /*argc*/, char* /*argv*/ []) {
 
     using dbn_t = dll::dbn_desc<
         dll::dbn_layers<
-            dll::conv_desc<1, 28, 28, 8, 5, 5>::layer_t,
-            dll::mp_layer_3d_desc<8, 24, 24, 1, 2, 2, dll::weight_type<float>>::layer_t,
-            dll::conv_desc<8, 12, 12, 8, 5, 5>::layer_t,
-            dll::mp_layer_3d_desc<8, 8, 8, 1, 2, 2, dll::weight_type<float>>::layer_t,
-            dll::dense_desc<8 * 4 * 4, 150>::layer_t,
-            dll::dense_desc<150, 10, dll::activation<dll::function::SOFTMAX>>::layer_t>,
+            dll::conv_layer<1, 28, 28, 8, 5, 5>,
+            dll::mp_3d_layer<8, 24, 24, 1, 2, 2>,
+            dll::conv_layer<8, 12, 12, 8, 5, 5>,
+            dll::mp_3d_layer<8, 8, 8, 1, 2, 2>,
+            dll::dense_layer<8 * 4 * 4, 150>,
+            dll::dense_layer<150, 10, dll::softmax>>,
         dll::updater<dll::updater_type::MOMENTUM>,
         dll::batch_size<100>
         >::dbn_t;
